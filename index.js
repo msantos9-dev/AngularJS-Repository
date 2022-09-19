87e2a03d90a88f876aea3a42986ae050a7811ee9
msantos9-dev <112072332+msantos9-dev@users.noreply.github.com>
2022-09-20 00:12:39 +0800

    EXER 1 2 TO 4 IS OKAY 1 IS PENDING

--- index.js
+++ index.js
@@ -1,8 +1,91 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.testFunction = void 0;
-const testFunction = () => {
-    console.log("Hello to Typescript");
+exports.displayCities = exports.searchCities = exports.addCities = exports.addCity = void 0;
+console.log("");
+var myCities = [];
+const addCity = (...citiesArray) => {
+    citiesArray.forEach((item) => {
+        console.log(item);
+    });
 };
-exports.testFunction = testFunction;
-(0, exports.testFunction)();
+exports.addCity = addCity;
+const addCities = (city) => {
+    myCities.push(city);
+    localStorage.setItem(myCities.length.toString(), city);
+    (0, exports.displayCities)();
+};
+exports.addCities = addCities;
+const searchCities = () => {
+    var _a;
+    const input = document.getElementById('search');
+    const value = input === null || input === void 0 ? void 0 : input.value;
+    console.log(value);
+    console.log((_a = localStorage.getItem("")) === null || _a === void 0 ? void 0 : _a.includes(value));
+};
+exports.searchCities = searchCities;
+const displayCities = () => {
+    let list = document.getElementById('cityList');
+    if (list) {
+        while (list.hasChildNodes() && list.firstChild) {
+            list.removeChild(list.firstChild);
+        }
+    }
+    myCities.forEach((item) => {
+        if (list) {
+            let li = document.createElement("li");
+            li.innerText = item;
+            list.appendChild(li);
+        }
+    });
+};
+exports.displayCities = displayCities;
+const array = ['a', '0', 'c', '0', 'Marvin', 'Jose', '0', 'ABC', '0', 'e', 'a', '0', '123', '0', 'e'];
+console.log("START ARRAY: " + array);
+let newArray = [];
+for (let i = array.length - 1; i >= 0; i--) {
+    if (array[i] === '0') {
+        newArray = moveItem(array, i, array.length - 1);
+    }
+}
+function moveItem(arr, fromIndex, toIndex) {
+    let itemRemoved = arr.splice(fromIndex, 1);
+    arr.splice(toIndex, 0, itemRemoved[0]);
+    return arr;
+}
+console.log("END ARRAY: " + newArray);
+function changer(s) {
+    s = s.toLowerCase();
+    return s.split(' ').map(word => word.split('').map(letter => {
+        if (letter === 'z')
+            return 'A';
+        if (letter === '0')
+            return '0';
+        let x = parseInt(letter) ? letter : String.fromCharCode(letter.charCodeAt(letter.length - 1) + 1);
+        if (/([aeiou])/g.test(x))
+            return x.toUpperCase();
+        return x;
+    }).join('')).join(' ');
+}
+console.log("FROM: " + "Hello World 123");
+console.log("TO: " + changer("Hello World 123"));
+function validISBN10(isbn) {
+    let index = 0;
+    let checkSum = 0;
+    let xCount = 0;
+    for (; index < isbn.length; index++) {
+        if (isbn[index] === 'X') {
+            xCount += 1;
+        }
+        checkSum += parseInt(isbn[index]) * (index + 1);
+    }
+    if (isbn.length !== 10) {
+        return false;
+    }
+    if (isbn[isbn.length - 1] === 'X' && xCount === 1) {
+        return true;
+    }
+    return checkSum % 11 === 0;
+}
+console.log("IS VALID: 23677828X: " + validISBN10("123677828X"));
+console.log("IS VALID: 236X77828: " + validISBN10("123677828X"));
+console.log("IS VALID: 2367828X: " + validISBN10("123677828X"));
--- index.ts
+++ index.ts
@@ -1,4 +1,113 @@
-export const testFunction = () => {
-    console.log("Hello to Typescript")
+
+
+console.log("")
+
+var myCities:string[] = [];
+export const addCity = (...citiesArray:string[]) => {
+    citiesArray.forEach((item) => {
+        console.log(item)
+    })
 }
-testFunction();
+
+
+export const addCities = (city:string) => {
+    myCities.push(city);
+    localStorage.setItem(myCities.length.toString(), city);
+    displayCities();
+}
+
+export const searchCities = () => {
+    const input = document.getElementById('search') as HTMLInputElement;
+    const value = input?.value;
+    console.log(value)
+
+    console.log(localStorage.getItem("")?.includes(value));
+    
+}
+
+export const displayCities = () => {
+
+    let list = document.getElementById('cityList');
+
+
+    if(list) {
+        while(list.hasChildNodes() && list.firstChild) {
+            list.removeChild(list.firstChild)
+        }
+    }
+    myCities.forEach((item) => {
+        if(list) {
+            let li = document.createElement("li");
+            li.innerText = item
+            list.appendChild(li)
+        }
+    })
+}
+
+
+
+const array = ['a', '0', 'c', '0', 'Marvin','Jose', '0', 'ABC', '0', 'e','a', '0', '123', '0', 'e']
+console.log("START ARRAY: " + array)
+
+let newArray: any[] = [];
+
+for(let i = array.length - 1; i >= 0; i--) {
+    if(array[i] === '0'){
+        newArray = moveItem(array, i, array.length - 1);
+    }
+}
+
+function moveItem(arr: any[], fromIndex: number, toIndex: number){
+  let itemRemoved = arr.splice(fromIndex, 1) 
+  arr.splice(toIndex, 0, itemRemoved[0]) 
+  return arr
+}
+
+console.log("END ARRAY: " + newArray)
+
+
+function changer(s: string) { 
+    s= s.toLowerCase()
+  
+    return s.split(' ').map(word=>
+  
+      word.split('').map(letter=>{
+        if(letter === 'z' ) return 'A'
+        if(letter === '0')  return '0'
+        let x = parseInt(letter) ? letter : String.fromCharCode(letter.charCodeAt(letter.length - 1) + 1)
+        if(/([aeiou])/g.test(x)) return x.toUpperCase()
+        return x
+      }).join('')
+    ).join(' ')
+  }
+
+  console.log("FROM: " + "Hello World 123")
+  console.log("TO: " + changer("Hello World 123"))
+
+
+
+  function validISBN10(isbn: string | any[]) {
+    let index = 0;
+    let checkSum = 0;
+    let xCount = 0;
+    for (; index < isbn.length; index++) {
+      if (isbn[index] === 'X') {
+        xCount += 1;
+      }
+      checkSum += parseInt(isbn[index]) * (index+1);
+    }
+  
+    if (isbn.length !== 10) {
+      return false;
+    }
+  
+    if (isbn[isbn.length-1] === 'X' && xCount === 1) {
+      return true;
+    }
+  
+    return checkSum % 11 === 0;
+  }
+
+  console.log("IS VALID: 23677828X: " + validISBN10("123677828X"))
+  console.log("IS VALID: 236X77828: " + validISBN10("123677828X"))
+  console.log("IS VALID: 2367828X: " + validISBN10("123677828X"))
